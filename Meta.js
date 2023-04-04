2c3eb26d2ecf061742fd69a923de9a30a03f5a2b
Roberto <104776273+CyberNinjaH@users.noreply.github.com>
2023-04-04 07:19:11 -0700

    Add files via upload

---
+++ Meta.js
@@ -0,0 +1,12 @@
+
+const Meta = () => {
+    <>
+        <meta name="title" content="Orban Robert - Software Developer"></meta>
+        <meta name="description" content="My personal website and portifolio."></meta>
+        <meta name="keywords" content="developer, front-end, programmer, portifolio, nextjs, react, tailwind, hire"></meta>
+        <meta name="robots" content="index, follow"></meta>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"></meta>
+        <meta name="language" content="English"></meta>
+    </>
+}
+export default Meta
