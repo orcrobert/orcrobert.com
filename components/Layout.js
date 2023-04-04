9396c6d4ec2ed0d939de7936dd2de879c7381b80
Roberto <104776273+CyberNinjaH@users.noreply.github.com>
2023-04-04 07:21:16 -0700

    Add files via upload

--- components/Layout.js
+++ components/Layout.js
@@ -3,12 +3,14 @@ import Head from "next/head";
 import Header from '../pages/Header';
 import Footer from '../pages/Footer';
 import Hero from '../pages/Hero';
+import Meta from "./Meta";
 
 const Layout = ({ children }) => {
     return (
         <>
             <Head>
                 <title>Orban Robert</title>
+                <Meta />
             </Head>
             <div className="min-h-screen flex flex-col">
                 <Header />
