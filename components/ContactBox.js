fe04cb82168ab9aa90a6cea2c50ebdea5dce6f89
orcrobert <robyorban@gmail.com>
2025-03-04 19:41:25 +0200

    update cv

--- components/ContactBox.js
+++ components/ContactBox.js
@@ -58,7 +58,7 @@ const ContactBox = () => {
                         </ul>
                         <ul className="flex gap-12 pt-3">
                             <li>
-                                <Link href={"https://drive.google.com/file/d/1GXRc_yRcW72wQ9llxh1liuiXO2Xqs3z1/view?usp=drive_link"} className="flex gap-1">
+                                <Link href={"https://drive.google.com/file/d/1KKaIGXupNQwaWlS13F3rL59LUHqcFuL6/view?usp=sharing"} className="flex gap-1">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                         <path d="M3.375 3C2.339 3 1.5 3.84 1.5 4.875v.75c0 1.036.84 1.875 1.875 1.875h17.25c1.035 0 1.875-.84 1.875-1.875v-.75C22.5 3.839 21.66 3 20.625 3H3.375z" />
                                         <path fillRule="evenodd" d="M3.087 9l.54 9.176A3 3 0 006.62 21h10.757a3 3 0 002.995-2.824L20.913 9H3.087zm6.163 3.75A.75.75 0 0110 12h4a.75.75 0 010 1.5h-4a.75.75 0 01-.75-.75z" clipRule="evenodd" />
--- pages/Hero.js
+++ pages/Hero.js
@@ -13,7 +13,7 @@ const Hero = () => {
 
     const handleViewCVClick = () => {
         window.location.href =
-            "https://drive.google.com/file/d/1GXRc_yRcW72wQ9llxh1liuiXO2Xqs3z1/view?usp=drive_link";
+            "https://drive.google.com/file/d/1KKaIGXupNQwaWlS13F3rL59LUHqcFuL6/view?usp=sharing";
     };
     const handleWriteMeClick = () => {
         window.location.href = "mailto:robyorban@gmail.com";
@@ -50,10 +50,9 @@ const Hero = () => {
             ></div>
             <div
                 className="
-          rounded-lg h-auto w-full md:w-96 p-4 bg-gray-200 dark:bg-zinc-800
-          shadow-md relative md:hover:transition md:hover:ease-in-out md:hover:delay-75
-          md:hover:scale-105 dark:text-zinc-100
-        "
+                rounded-lg h-auto w-full md:w-96 p-4 bg-gray-200 dark:bg-zinc-800
+                shadow-md relative md:hover:transition md:hover:ease-in-out md:hover:delay-75
+                md:hover:scale-105 dark:text-zinc-100"
         style={{ willChange: "filter, transform" }}
             >
                 <div className="flex items-center">
@@ -63,8 +62,10 @@ const Hero = () => {
                             Orban Robert
                         </strong>
                         <div className="flex items-center">
-                            {/* SVG Icon */}
-                            <h4 className="text-sm sm:text-base">Software Developer</h4>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#78b13f" className="w-5 h-5 mr-1">
+                        <path fillRule="evenodd" d="M8.603 3.799A4.49 4.49 0 0112 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 013.498 1.307 4.491 4.491 0 011.307 3.497A4.49 4.49 0 0121.75 12a4.49 4.49 0 01-1.549 3.397 4.491 4.491 0 01-1.307 3.497 4.491 4.491 0 01-3.497 1.307A4.49 4.49 0 0112 21.75a4.49 4.49 0 01-3.397-1.549 4.49 4.49 0 01-3.498-1.306 4.491 4.491 0 01-1.307-3.498A4.49 4.49 0 012.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 011.307-3.497 4.49 4.49 0 013.497-1.307zm7.007 6.387a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+                        </svg>
+                            <h4 className="text-sm sm:text-base">Software Engineer</h4>
                         </div>
                     </div>
                 </div>
